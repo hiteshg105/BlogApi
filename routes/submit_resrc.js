08352956578905df5d68f1d6e098cc08a042fc1b
bhadresh lathiya <bhadreshl.smarttechnica@gmail.com>
2023-06-07 17:03:57 +0530

    work on creteor content

--- routes/submit_resrc.js
+++ routes/submit_resrc.js
@@ -4,44 +4,45 @@ const multer = require("multer");
 const fs = require("fs");
 
 const {
-    addSub_resrc,
-    admin_Sub_resrc,
-    user_sub_res_lsit,
-    admin_sub_res_lsit,
-    getone_reslist,
-    edit_submit_rsrc,
-    dlt_subres_list,
-    approve_submit_resrc,
-    listbycategory,
-    listbysubcategory,
-    Promotions,
-    total_sub_resrc,
-    total_paid_resrc,
-    total_free_resrc,
-    getone_submitresrc_list,
-    my_content_meteros,
-    App_Sub_resrc,
-    filter_type,
-    filterbypaid_subresrc,
-    filterbyFormat,
-    search_topic_title,
-    filterbyyear,
-    filterbyLanguage,
-    treding_topics,
-    filterbyHashTag,
-    posted_by_me,
-    edit_promotion,
-    filterbyid,
-    advancefilter,
-    hashfilter,
-    filter,
-    regidnamemobemail,
-    promotion_filter,
-    search_promotion,
-    search_filter,
-    keyword_search_filter,
-    promotion_search_filter
-    
+  addSub_resrc,
+  admin_Sub_resrc,
+  user_sub_res_lsit,
+  admin_sub_res_lsit,
+  getone_reslist,
+  edit_submit_rsrc,
+  dlt_subres_list,
+  approve_submit_resrc,
+  listbycategory,
+  listbysubcategory,
+  Promotions,
+  total_sub_resrc,
+  total_paid_resrc,
+  total_free_resrc,
+  getone_submitresrc_list,
+  my_content_meteros,
+  App_Sub_resrc,
+  filter_type,
+  filterbypaid_subresrc,
+  filterbyFormat,
+  search_topic_title,
+  filterbyyear,
+  filterbyLanguage,
+  treding_topics,
+  filterbyHashTag,
+  posted_by_me,
+  edit_promotion,
+  filterbyid,
+  advancefilter,
+  hashfilter,
+  filter,
+  regidnamemobemail,
+  promotion_filter,
+  search_promotion,
+  search_filter,
+  keyword_search_filter,
+  promotion_search_filter,
+  active_resrc_lsit
+
 } = require("../controller/submit_resrc");
 
 if (!fs.existsSync("./uploads")) {
@@ -58,35 +59,35 @@ const storage = multer.diskStorage({
   },
 });
 
-  let uploads = multer({ storage: storage });
-  
-  let multipleUpload = uploads.fields([
-    { name: "img", maxCount: 1 },
-   
-    //   { name: "storepan_img", maxCount: 5 },
-    
-  ]);
-  
- 
- router.post("/user/addSub_resrc",uploads.single("img"), addSub_resrc);
- router.post("/user/App_Sub_resrc",uploads.single("img"), App_Sub_resrc);
+let uploads = multer({ storage: storage });
 
- router.post("/admin/admin_Sub_resrc",uploads.single("img"), admin_Sub_resrc);
+let multipleUpload = uploads.fields([
+  { name: "img", maxCount: 1 },
 
- router.get("/user/user_sub_res_lsit", user_sub_res_lsit);
- router.get("/admin/admin_sub_res_lsit", admin_sub_res_lsit);
+  //   { name: "storepan_img", maxCount: 5 },
 
- router.get("/admin/getone_reslist/:id", getone_reslist);
-  router.post("/admin/edit_submit_rsrc/:id",multipleUpload, edit_submit_rsrc);
- router.get("/admin/dlt_subres_list/:id", dlt_subres_list);
- router.post("/admin/approve_submit_resrc/:id", approve_submit_resrc);
- router.get("/admin/listbycategory/:id", listbycategory);
- router.get("/admin/listbysubcategory/:id", listbysubcategory);
- router.get("/user/Promotions", Promotions);
- router.get("/admin/total_sub_resrc", total_sub_resrc);
- router.get("/admin/total_paid_resrc", total_paid_resrc);
+]);
 
- router.get("/admin/total_free_resrc", total_free_resrc);
+
+router.post("/user/addSub_resrc", uploads.single("img"), addSub_resrc);
+router.post("/user/App_Sub_resrc", uploads.single("img"), App_Sub_resrc);
+
+router.post("/admin/admin_Sub_resrc", uploads.single("img"), admin_Sub_resrc);
+
+router.get("/user/user_sub_res_lsit", user_sub_res_lsit);
+router.get("/admin/admin_sub_res_lsit", admin_sub_res_lsit);
+
+router.get("/admin/getone_reslist/:id", getone_reslist);
+router.post("/admin/edit_submit_rsrc/:id", multipleUpload, edit_submit_rsrc);
+router.get("/admin/dlt_subres_list/:id", dlt_subres_list);
+router.post("/admin/approve_submit_resrc/:id", approve_submit_resrc);
+router.get("/admin/listbycategory/:id", listbycategory);
+router.get("/admin/listbysubcategory/:id", listbysubcategory);
+router.get("/user/Promotions", Promotions);
+router.get("/admin/total_sub_resrc", total_sub_resrc);
+router.get("/admin/total_paid_resrc", total_paid_resrc);
+
+router.get("/admin/total_free_resrc", total_free_resrc);
 // router.get("/user/getone_submitresrc_list", getone_submitresrc_list);
 router.get("/user/my_content_meteros/:id", my_content_meteros);
 router.get("/user/filter_type/:sub_category/:id", filter_type);
@@ -115,6 +116,9 @@ router.post("/user/search_promotion", search_promotion);
 router.post("/user/search_filter", search_filter);
 router.post("/user/keyword_search_filter", keyword_search_filter);
 router.post("/user/promotion_search_filter", promotion_search_filter);
+
+
+router.get("/user/get_all_active_resrc_lsit", active_resrc_lsit)
 
 
 
