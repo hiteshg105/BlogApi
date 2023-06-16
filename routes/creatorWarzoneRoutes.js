d6f3553a2eb8439f517a668d30a7991b91ad639b
bhadresh lathiya <bhadreshl.smarttechnica@gmail.com>
2023-06-16 13:21:49 +0530

    work on creator content warzone controller

--- routes/creatorWarzoneRoutes.js
+++ routes/creatorWarzoneRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { addCreatorWar, creator_warZone_list, getAllCreatorWar, getCreatorWarDetail, deleteCreatorWarzone, updateCreatorWarzone, creatoWarReview } = require("../controller/creatroWarzoneController");
+const { addCreatorWar, creator_warZone_list, getAllCreatorWar, getCreatorWarDetail, deleteCreatorWarzone, updateCreatorWarzone, creatoWarReview, creatorWarRscsReview, creatorWarRscsComment, CreatorDeclareWinner, creatorWarRscsReviewAll, getAdminWarCreator } = require("../controller/creatroWarzoneController");
 const router = express.Router();
 
 router.post("/add/creator_warzone", addCreatorWar);
@@ -9,6 +9,12 @@ router.get("/get/single/creator_warzone/:id", getCreatorWarDetail);
 router.delete("/delete/creator_warzone/:id", deleteCreatorWarzone);
 router.put("/update/creator_warzone/:id", updateCreatorWarzone);
 router.get("/get/review/creator_warzone/:id", creatoWarReview);
+router.get("/get/allreview/creator_warzone/resource/:id", creatorWarRscsReview);
+router.get("/get/comment/creator_warzone/:id", creatorWarRscsComment);
+router.get("/creator_warzone/result/cron-job", CreatorDeclareWinner);
+router.get("/get/all/resource/creator_warzone", creatorWarRscsReviewAll);
+router.get("/creator_warzone/admin", getAdminWarCreator);
+
 
 
 
